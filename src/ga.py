5e805bc5a875e4c4afdf396ec8ac15483d2b50e9
Brandon J <brcjacob@ucsc.edu>
2020-02-18 14:38:12 -0800

    General mutation added

--- src/ga.py
+++ src/ga.py
@@ -73,7 +73,15 @@ class Individual_Grid(object):
         right = width - 1
         for y in range(height):
             for x in range(left, right):
-                pass
+                #10 percent chance to mutate individual
+                if random.random() < 0.1 and len(genome) > 0:
+                    #20% weight to mutate individual into a block
+                    if random.random() < 0.2:
+                        genome[y][x] = random.choice("X?MB")
+                    #80% weight to mutate individual into a coin
+                    else:
+                        genome[y][x] = "o"
+
         return genome
 
     # Create zero or more children from self and other
